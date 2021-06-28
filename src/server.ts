10dceff4e35050682370c0b1cc5a6547b88d2d86
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-06-28 14:05:46 -0300

    fix: teste
     Changes to be committed	modified:   .releaserc
    	modified:   package.json	modified:   src/server.ts
    	modified:   yarn.lock
    
    foot: uhsauhsuahsua issues#1

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello Quero', version: packageJson.version })
-  console.log('testeeee')
+  console.log('testee')
 })
 
 app.listen(3333);

f8b73833704e8df39c3090acc8e70ae46c9ef241
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-06-28 10:44:51 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import packageJson from '../package.json' //para ver a versão
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello Quero', version: packageJson.version })
+})
+
+app.listen(3333);
